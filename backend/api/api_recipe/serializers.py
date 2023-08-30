8f7d03130159f644c53f615c4ea74b6266b755d3
Yuliya Kryuchkova <kissanni@mail.ru>
2023-08-30 15:09:35 +0700

    foodgram

--- backend/api/api_recipe/serializers.py
+++ backend/api/api_recipe/serializers.py
@@ -6,6 +6,8 @@ from api.api_users.serializers import CustomUserSerializer
 from ingredient.models import Ingredient
 from recipes.models import IngredientRecipe, Recipe
 
+from favoriterecipe.models import FavoriteRecipe
+
 
 class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
@@ -59,7 +61,7 @@ class RecipeRetriveListSerializer(serializers.ModelSerializer):
     author = CustomUserSerializer()
     tags = TagSerializer(many=True)
     ingredients = serializers.SerializerMethodField()
-    # favorites = serializers.SerializerMethodField()
+    favorites = serializers.SerializerMethodField()
 
     class Meta:
         model = Recipe
@@ -68,7 +70,7 @@ class RecipeRetriveListSerializer(serializers.ModelSerializer):
                   'author',
                   'ingredients',
                   'is_favorited',
-                  # 'favorites'
+                  'favorites'
                   'name',
                   'image',
                   'text',
@@ -82,9 +84,9 @@ class RecipeRetriveListSerializer(serializers.ModelSerializer):
             user=self.context['request'].user
         ).exists()
 
-    # def get_favorites(self, obj):
-    #     favorites = FavoriteRecipe.objects.filter(recipe=obj)
-    #     return FavoriteSerializer(favorites, many=True).data
+    def get_favorites(self, obj):
+        favorites = FavoriteRecipe.objects.filter(recipe=obj)
+        return FavoriteSerializer(favorites, many=True).data
 
     def get_is_in_shopping_cart(self, obj):
         if not self.context['request'].user.is_authenticated:
@@ -175,3 +177,28 @@ class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
                 'Ингридиент уже в списке'
             )
         return values
+
+
+class FavoriteSerializer(serializers.ModelSerializer):
+    # recipe = RecipeRetriveListSerializer()
+
+    class Meta:
+        model = FavoriteRecipe
+        fields = (
+            'user',
+            'recipe',
+        )
+
+    def validate(self, data):
+        user = data['user']
+        if user.is_favorited.filter(recipe=data['recipe']).exists():
+            raise serializers.ValidationError(
+                'Рецепт в избранном.'
+            )
+        return data
+
+    def to_representation(self, instance):
+        return RecipeSerializer(
+            instance.recipe,
+            context={'request': self.context.get('request')}
+        ).data
