7ec4fa08fd0c68bd3b0e62f996b551a03d0ac7d2
Yuliya Kryuchkova <kissanni@mail.ru>
2023-09-03 19:27:48 +0700

    foodgram

--- backend/api/api_ingredient/filters.py
+++ backend/api/api_ingredient/filters.py
@@ -1,11 +1,11 @@
 from rest_framework.filters import SearchFilter
 
-from ingredient.models import Ingredient
+# from ingredient.models import Ingredient
 
 
 class IngredientFilter(SearchFilter):
     search_param = 'name'
 
-    class Meta:
-        model = Ingredient
-        fields = ('name',)
+    # class Meta:
+    #     model = Ingredient
+    #     fields = ('name',)
