0626c02c287866942bcdcb7fa07089f2298d0135
Yuliya Kryuchkova <kissanni@mail.ru>
2023-09-03 17:20:17 +0700

    foodgram

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -14,7 +14,8 @@ class Recipe(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='recipes',
-        verbose_name='Автор')
+        verbose_name='Автор'
+    )
     name = models.CharField(
         'Название рецепта',
         max_length=150
@@ -41,7 +42,8 @@ class Recipe(models.Model):
         validators=(
             MinValueValidator(
                 1,
-                message='Время приготовления не менее 1 минуты'),
+                message='Время приготовления не менее 1 минуты'
+            ),
         )
     )
     pub_date = models.DateTimeField(
@@ -51,7 +53,9 @@ class Recipe(models.Model):
     )
 
     class Meta:
-        ordering = ('-pub_date',)
+        ordering = (
+            '-pub_date',
+        )
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
 
@@ -67,13 +71,17 @@ class IngredientRecipe(models.Model):
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name='ingredient_recipes')
+        related_name='ingredient_recipes'
+    )
     amount = models.PositiveSmallIntegerField(
         default=1,
         validators=(
             validators.MinValueValidator(
-                1, message='Минимальное количество ингридиентов'),),
-        verbose_name='Количество', )
+                1, message='Минимальное количество ингридиентов'
+            ),
+        ),
+        verbose_name='Количество',
+    )
 
     def __str__(self):
         return f'{self.recipe.name} - {self.ingredient.name}'
