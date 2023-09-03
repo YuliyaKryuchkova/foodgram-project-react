eb8fbcb064b6c828251246d7de52a9c7f11d6910
Yuliya Kryuchkova <kissanni@mail.ru>
2023-09-03 15:01:03 +0700

    foodgram

--- backend/api/api_ingredient/views.py
+++ backend/api/api_ingredient/views.py
@@ -1,13 +1,15 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
 from ingredient.models import Ingredient
-from .filters import IngredientFilter
+# from .filters import IngredientFilter
 from .serializers import IngredientSerializer
+from permissions import IsAuthorOrReadOnly
 
 
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-    permission_classes = []
-    filter_backends = [IngredientFilter]
+    permission_classes = [IsAuthorOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
     search_fields = ('^name',)
