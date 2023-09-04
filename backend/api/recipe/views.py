250b044425156498fcd9859904777b15e90a65a0
Yuliya Kryuchkova <kissanni@mail.ru>
2023-09-04 16:30:58 +0700

    foodgram

--- backend/api/recipe/views.py
+++ backend/api/recipe/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Sum
-from django.http import JsonResponse
+from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.decorators import action
@@ -27,36 +27,23 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return RecipeRetriveListSerializer
         return RecipeCreateUpdateSerializer
 
-    # def message_shopping_cart(self, ingredients):
-    #     shopping_list = 'Список продуктов:'
-    #     for ingredient in ingredients:
-    #         shopping_list += (
-    #             f"\n{ingredient['ingredient__name']} "
-    #             f"({ingredient['ingredient__measurement_unit']}) - "
-    #             f"{ingredient['amount']}")
-    #     file = 'shopping_list.txt'
-    #     response = HttpResponse(shopping_list, content_type='text/plain')
-    #     response['Content-Disposition'] = f'attachment; filename="{file}.txt"
-    #     return response
-    #
-    # @action(detail=False, methods=['GET'])
-    # def download_shopping_cart(self, request):
-    #     ingredients = IngredientRecipe.objects.filter(
-    #         recipe__shoppingcart__user=request.user
-    #     ).order_by('ingredient__name').values(
-    #         'ingredient__name', 'ingredient__measurement_unit'
-    #     ).annotate(amount=Sum('amount'))
-    #     return self.message_shopping_cart(ingredients)
+    def message_shopping_cart(self, ingredients):
+        shopping_list = 'Список продуктов:'
+        for ingredient in ingredients:
+            shopping_list += (
+                f"\n{ingredient['ingredient__name']} "
+                f"({ingredient['ingredient__measurement_unit']}) - "
+                f"{ingredient['amount']}")
+        file = 'shopping_list.txt'
+        response = HttpResponse(shopping_list, content_type='text/plain')
+        response['Content-Disposition'] = f'attachment; filename="{file}.txt"'
+        return response
+
     @action(detail=False, methods=['GET'])
-    def download_shopping_cart(request):
+    def download_shopping_cart(self, request):
         ingredients = IngredientRecipe.objects.filter(
             recipe__shoppingcart__user=request.user
         ).order_by('ingredient__name').values(
             'ingredient__name', 'ingredient__measurement_unit'
         ).annotate(amount=Sum('amount'))
-
-        shopping_cart_data = {
-            'ingredients': list(ingredients)
-        }
-
-        return JsonResponse(shopping_cart_data)
+        return self.message_shopping_cart(ingredients)
