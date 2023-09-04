f19b352b12bec3a1cc185342b34e02fb62ad3751
Yuliya Kryuchkova <kissanni@mail.ru>
2023-09-04 22:39:54 +0700

    foodgram

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,9 +6,11 @@ app_name = 'api'
 urlpatterns = [
     path('', include('api.tag.urls')),
     path('', include('api.subscribe.urls')),
-    path('', include('api.users.urls')),
     path('', include('api.shopcart.urls')),
-    path('', include('api.recipe.urls')),
+
     path('', include('api.ingredient.urls')),
     path('', include('api.favorite.urls')),
+    path('', include('api.recipe.urls')),
+    path('', include('api.users.urls')),
+
 ]
--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -70,20 +70,14 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 AUTH_USER_MODEL = 'users.User'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': env('POSTGRES_DB', 'django'),
-#         'USER': env('POSTGRES_USER', 'django'),
-#         'PASSWORD': env('POSTGRES_PASSWORD', ''),
-#         'HOST': env('DB_HOST', ''),
-#         'PORT': env('DB_PORT', 5432)
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('POSTGRES_DB', 'django'),
+        'USER': env('POSTGRES_USER', 'django'),
+        'PASSWORD': env('POSTGRES_PASSWORD', ''),
+        'HOST': env('DB_HOST', ''),
+        'PORT': env('DB_PORT', 5432)
     }
 }
 
