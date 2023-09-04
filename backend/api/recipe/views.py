3c8e1a3a8fa069b62faf5224aa2bb497ec0cbad5
Yuliya Kryuchkova <kissanni@mail.ru>
2023-09-04 16:11:10 +0700

    foodgram

--- backend/api/recipe/views.py
+++ backend/api/recipe/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Sum
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.decorators import action
